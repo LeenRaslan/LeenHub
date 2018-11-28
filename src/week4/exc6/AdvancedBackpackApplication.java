36f5ae850ee643d1830ff3395df676b46b0928ca
LeenRaslan <leen.b.r.357@gmail.com>
2018-11-28 16:36:07 +0100

    Push finally

--- src/week4/exc6/AdvancedBackpackApplication.java
+++ src/week4/exc6/AdvancedBackpackApplication.java
@@ -1,4 +1,44 @@
 package week4.exc6;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class AdvancedBackpackApplication {
+    public static void main(String[] args) {
+
+        Set<Item> backpack = new HashSet<>();
+        System.out.println("Backpack: " + backpack);
+
+        Item item1 = new Item("Water");
+        Item item2 = new Item("Food");
+        Item item3 = new Item("Medicine");
+
+        backpack.addAll(Arrays.asList(item1,item2,item3));
+        System.out.println("Backpack: " + backpack);
+
+        Item item4 = new Item("Water");
+        //if (backpack.contains(item4)){
+        boolean containsItem = backpack.contains(item4);
+        if (containsItem){
+            System.out.println("The backpack contains " + item4);}
+        backpack.remove(item4);
+        System.out.println(backpack);
+        if (!backpack.contains(item4)){
+            System.out.println(backpack);
+        }else System.out.println("");
+
+        backpack.addAll(Arrays.asList(item2,item2));
+        System.out.println(backpack);
+
+        int size = backpack.size();
+        System.out.println("backpack size: " + size);
+
+
+
+
+    }
+
+
+
 }
--- src/week4/exc7/AdvancedIndexApplication.java
+++ src/week4/exc7/AdvancedIndexApplication.java
@@ -1,4 +1,49 @@
 package week4.exc7;
 
+import java.util.*;
+
 public class AdvancedIndexApplication {
+
+    public static void main(String[] args) {
+
+        Map<Integer , List<Topic>> bookTopics = new HashMap<>();
+        System.out.println("Index: " + bookTopics);
+
+        Integer page1 = 3;
+        Integer page2 = 7;
+
+        Topic topic1 = new Topic("connecting");
+        Topic topic2 = new Topic("possitive things");
+        Topic topic3 = new Topic("ecologic economy");
+
+        List<Topic> topics1 = new ArrayList<>();
+        List<Topic> topics2 = new ArrayList<>();
+
+        topics1.add(topic1);
+        topics2.addAll(Arrays.asList(topic2,topic3));
+
+        bookTopics.put(page1,topics1);
+        bookTopics.put(page2,topics2);
+        System.out.println("Index: " + bookTopics);
+
+        Set<Integer> pages = bookTopics.keySet();
+        System.out.println("Index keys: " +pages);
+
+        Collection<List<Topic>> topics = bookTopics.values();
+        System.out.println("Index values: " + topics);
+
+        Topic newTopic = new Topic("teaching methods");
+
+        List<Topic> stored = bookTopics.get(3);
+        stored.add(newTopic);
+        System.out.println(bookTopics);
+
+        boolean containsKey = bookTopics.containsKey(3);
+        if (containsKey){
+            System.out.println("The page number "+ page1 +" exists as key.");
+        }else System.out.println("");
+
+
+
+    }
 }
