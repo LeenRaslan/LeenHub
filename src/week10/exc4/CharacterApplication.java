35324e33873ef99bbc6f0a30ad3de8901330eac1
LeenRaslan <leen.b.r.357@gmail.com>
2019-01-24 17:45:42 +0100

    week10

--- src/week10/exc4/CharacterApplication.java
+++ src/week10/exc4/CharacterApplication.java
@@ -2,17 +2,15 @@ package week10.exc4;
 
 public class CharacterApplication {
     public static void main(String[] args) {
-        CharacterSummarizer summarizer = new CharacterSummarizer();
-        System.out.println("Total namber of characters appear in the books: ");
-        System.out.println(summarizer.getCharacters());
 
-        System.out.println("Total number of dead characters: ");
-        System.out.println(summarizer.getDeadCharacters());
+        CharacterSummarizer summarizer = new CharacterSummarizer();
+        System.out.println("Total namber of characters appear in the books: " +summarizer.getCharacters());
+
+        System.out.println("Total number of dead characters: "+summarizer.getDeadCharacters());
 
         System.out.println("Percentage of men and women that died in all books: ");
         System.out.println(summarizer.getPercentageOfDeadChar());
         System.out.println("\n");
-
 
         System.out.println(summarizer.getBiggestDeathCount());
 
@@ -23,18 +21,25 @@ public class CharacterApplication {
         System.out.println("Names of the two allegiances that have the biggest dead count: ");
         summarizer.getTwoBiggestAllegiance().forEach(System.out::println);
 
+        System.out.println("percentage of deaths belong to nobility characters: ");
         System.out.println(summarizer.getNobilityDeathPercentage());
 
+        System.out.println("most amount of dead characters from the Stark allegiance: ");
         System.out.println(summarizer.getMostStarkAlligiance());
 
+        System.out.println("most amount of dead characters from the Lannister allegiance");
         System.out.println(summarizer.getMostLannisterAlligiance());
 
+        System.out.println("Number of characters in Starks have died");
         System.out.println(summarizer.countDiedStarks());
 
+        System.out.println("Number of characters in Lannister have died");
         System.out.println(summarizer.countDiedLannister());
 
+        System.out.println("Is there any character who didn’t die?");
         System.out.println(summarizer.getAliveCharacters());
 
+        System.out.println("Has any character ever been killed in the same chapter that it got introduced?");
         System.out.println(summarizer.getSameKilledTiming());
     }
 }
