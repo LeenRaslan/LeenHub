53b6402ed47b6af82dc339b831d59c5a22ef464a
LeenRaslan <43730200+LeenRaslan@users.noreply.github.com>
2019-02-01 10:49:55 +0100

    Delete SolsticeApp.java

--- src/reflection/week11/SolsticeApp.java
+++
@@ -1,8 +0,0 @@
-package reflection.week11;
-
-public class SolsticeApp {
-    public static void main(String[] args) {
-        Solstice solstice = new Solstice();
-        solstice.getSummerSolsticeCountdown(2018,2,15);
-    }
-}
